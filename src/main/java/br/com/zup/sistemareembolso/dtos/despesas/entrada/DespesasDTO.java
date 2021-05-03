ed80716ac8027d93c69599e5d1c96c87d68a46cf
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-05-03 18:22:03 -0300

    Criar DTO de entrada para as despesas

---
+++ src/main/java/br/com/zup/sistemareembolso/dtos/despesas/entrada/DespesasDTO.java
@@ -0,0 +1,51 @@
+package br.com.zup.sistemareembolso.dtos.despesas.entrada;
+import br.com.zup.sistemareembolso.models.Colaborador;
+import br.com.zup.sistemareembolso.models.Despesas;
+import br.com.zup.sistemareembolso.models.Status;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+public class DespesasDTO {
+
+    @CPF
+    private String cpf;
+
+    @NotBlank
+    private String descricao;
+
+    @Positive
+    private double valor;
+
+    private Status status;
+
+    public DespesasDTO() {}
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public double getValor() { return valor; }
+    public void setValor(double valor) { this.valor = valor; }
+
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
+
+
+    public Despesas converterDTOparaDespesas(){
+        Despesas despesas = new Despesas();
+
+        Colaborador colaborador = new Colaborador();
+        colaborador.setCpf(this.cpf);
+
+        despesas.setColaborador(colaborador);
+        despesas.setDescricao(this.descricao);
+        despesas.setValor(this.valor);
+        despesas.setStatus(this.status);
+
+        return despesas;
+    }
+}
