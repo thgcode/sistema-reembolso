0765c40e6621aff5a5b9f337145babd6c401bd54
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-05-03 18:25:57 -0300

    Criar repositório das Despesas

---
+++ src/main/java/br/com/zup/sistemareembolso/repositories/DespesasRepository.java
@@ -0,0 +1,8 @@
+package br.com.zup.sistemareembolso.repositories;
+
+import br.com.zup.sistemareembolso.models.Despesas;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DespesasRepository extends CrudRepository<Despesas, Integer> {
+
+}
