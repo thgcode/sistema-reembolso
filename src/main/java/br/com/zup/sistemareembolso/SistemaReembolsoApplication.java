676e7c064a1beabd94660c17b8aa222d89b64363
Thiago Seus <thiago.seus@yahoo.com.br>
2021-05-03 14:23:56 -0300

    Adicionar os arquivos do Spring Initializer

---
+++ src/main/java/br/com/zup/sistemareembolso/SistemaReembolsoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.sistemareembolso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReembolsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReembolsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/sistemareembolso/SistemaReembolsoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.sistemareembolso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReembolsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
