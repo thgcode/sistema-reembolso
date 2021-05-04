a3e1b0e50f1d1d04a7301c58f9054d29d698e9b6
Thiago Seus <thiago.seus@yahoo.com.br>
2021-05-04 11:42:29 -0300

    Adicionar a classe JWTConfig com a configuração para autorizar o endpoint de criar um colaborador

--- src/main/java/br/com/zup/sistemareembolso/config/JWTConfig.java
+++ src/main/java/br/com/zup/sistemareembolso/config/JWTConfig.java
@@ -1,2 +1,28 @@
-package br.com.zup.sistemareembolso.config;public class JWTConfig {
+package br.com.zup.sistemareembolso.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class JWTConfig extends WebSecurityConfigurerAdapter {
+    private static final String[] PUBLIC_MATCHERS_GET = {
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/colaboradores/",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+
+        http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                .anyRequest().authenticated();
+    }
 }
