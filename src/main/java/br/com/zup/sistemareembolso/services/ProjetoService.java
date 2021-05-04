0c53c87cfc2556a1ca090c70032e5a035345e5c6
Thiago Seus <thiago.seus@yahoo.com.br>
2021-05-04 16:31:25 -0300

    Adicionar método de pesquisar um projeto pelo ID

--- src/main/java/br/com/zup/sistemareembolso/services/ProjetoService.java
+++ src/main/java/br/com/zup/sistemareembolso/services/ProjetoService.java
@@ -1,9 +1,12 @@
 package br.com.zup.sistemareembolso.services;
 
+import br.com.zup.sistemareembolso.exceptions.ProjetoNaoExistenteException;
 import br.com.zup.sistemareembolso.models.Projeto;
 import br.com.zup.sistemareembolso.repositories.ProjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProjetoService {
@@ -16,5 +19,15 @@ public class ProjetoService {
 
     public Iterable <Projeto> listarProjetos() {
         return projetoRepository.findAll();
+    }
+
+    public Projeto pesquisarProjetoPeloId(int id) {
+        Optional <Projeto> optionalProjeto = projetoRepository.findById(id);
+
+        if (optionalProjeto.isPresent()) {
+            return optionalProjeto.get();
+        }
+
+        throw new ProjetoNaoExistenteException();
     }
 }
