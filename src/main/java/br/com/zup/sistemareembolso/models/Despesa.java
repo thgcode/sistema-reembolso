20878ded070139c2411edbaf0a501f414b2a7da4
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-05-04 16:13:17 -0300

    Resolver conflitos nas entidades Colaborador e Despesas

--- src/main/java/br/com/zup/sistemareembolso/models/Despesa.java
+++ src/main/java/br/com/zup/sistemareembolso/models/Despesa.java
@@ -3,6 +3,7 @@ package br.com.zup.sistemareembolso.models;
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
+import java.time.LocalDate;
 
 @Table(name = "despesas")
 @Entity
@@ -20,25 +21,14 @@ public class Despesa {
 
     @Column(nullable = false)
     private String descricao;
-
     private double valor;
-    private Date dataEntrada;
-    private Date dataAprovacao;
-
+    private LocalDate dataEntrada;
+    private LocalDate dataAprovacao;
     private Status status;
-
-    public List<Projeto> getProjeto() {
-        return projeto;
-    }
-
-    public void setProjeto(List<Projeto> projeto) {
-        this.projeto = projeto;
-    }
 
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -46,7 +36,6 @@ public class Despesa {
     public Colaborador getColaborador() {
         return colaborador;
     }
-
     public void setColaborador(Colaborador colaborador) {
         this.colaborador = colaborador;
     }
@@ -54,40 +43,28 @@ public class Despesa {
     public String getDescricao() {
         return descricao;
     }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
+    public double getValor() { return valor; }
     public void setValor(double valor) {
         this.valor = valor;
     }
 
-    public Date getDataEntrada() {
-        return dataEntrada;
-    }
+    public LocalDate getDataEntrada() { return dataEntrada; }
+    public void setDataEntrada(LocalDate dataEntrada) { this.dataEntrada = dataEntrada; }
 
-    public void setDataEntrada(Date dataEntrada) {
-        this.dataEntrada = dataEntrada;
-    }
-
-    public Date getDataAprovacao() {
-        return dataAprovacao;
-    }
-
-    public void setDataAprovacao(Date dataAprovacao) {
-        this.dataAprovacao = dataAprovacao;
-    }
+    public LocalDate getDataAprovacao() { return dataAprovacao; }
+    public void setDataAprovacao(LocalDate dataAprovacao) { this.dataAprovacao = dataAprovacao; }
 
     public Status getStatus() {
         return status;
     }
-
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    public List<Projeto> getProjeto() { return projeto; }
+    public void setProjeto(List<Projeto> projeto) {
+        this.projeto = projeto;
     }
 }
--- src/main/java/br/com/zup/sistemareembolso/services/DespesaService.java
+++ src/main/java/br/com/zup/sistemareembolso/services/DespesaService.java
@@ -1,17 +1,30 @@
 package br.com.zup.sistemareembolso.services;
 
+import br.com.zup.sistemareembolso.models.Colaborador;
 import br.com.zup.sistemareembolso.models.Despesa;
 import br.com.zup.sistemareembolso.repositories.DespesaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class DespesaService {
 
     @Autowired
     private DespesaRepository despesaRepository;
+    @Autowired
+    private ColaboradorService colaboradorService;
 
     public Despesa adicionarDespesa(Despesa despesa) {
+
+        despesa.setDataEntrada(LocalDate.now());
+
+        /* Verificar se existe o colaborador */
+        Colaborador colaborador = colaboradorService.pesquisarColaboradorPorCpf(despesa.getColaborador().getCpf());
+
+        despesa.setColaborador(colaborador);
+
         return despesaRepository.save(despesa);
     }
 
