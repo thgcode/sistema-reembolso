460af691ee75c2c4b4a17e520bb28285d02dea1e
Thiago Seus <thiago.seus@yahoo.com.br>
2021-05-06 08:50:51 -0300

    Corrigir o endpoint da localidade, que estava sem o s

--- src/main/java/br/com/zup/sistemareembolso/controllers/LocalidadeController.java
+++ src/main/java/br/com/zup/sistemareembolso/controllers/LocalidadeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("localidade/")
+@RequestMapping("localidades/")
 public class LocalidadeController {
     @Autowired
     private LocalidadeService localidadeService;
