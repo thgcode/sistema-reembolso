1a6fe9adb4f997cbbf94d495c9aec34edfc5d715
Thiago Seus <thiago.seus@yahoo.com.br>
2021-05-06 16:55:27 -0300

    Adicionar o repositório da nota fiscal

---
+++ src/main/java/br/com/zup/sistemareembolso/repositories/NotaFiscalRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.sistemareembolso.repositories;
+
+import br.com.zup.sistemareembolso.models.NotaFiscal;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NotaFiscalRepository extends CrudRepository <NotaFiscal, Integer> {
+}
