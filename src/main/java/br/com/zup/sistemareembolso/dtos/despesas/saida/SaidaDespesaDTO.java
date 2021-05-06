139ab1dbfe482a9e318cceb2ea8554d6c6b9e2ec
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-05-06 13:53:31 -0300

    Corrigir saida duplicada de projeto

--- src/main/java/br/com/zup/sistemareembolso/dtos/despesas/saida/SaidaDespesaDTO.java
+++ src/main/java/br/com/zup/sistemareembolso/dtos/despesas/saida/SaidaDespesaDTO.java
@@ -2,12 +2,10 @@ package br.com.zup.sistemareembolso.dtos.despesas.saida;
 
 import br.com.zup.sistemareembolso.models.Colaborador;
 import br.com.zup.sistemareembolso.models.Despesa;
-import br.com.zup.sistemareembolso.models.Projeto;
 import br.com.zup.sistemareembolso.models.Status;
 
 public class SaidaDespesaDTO {
     private Colaborador colaborador;
-    private Projeto projeto;
     private String descricao;
     private double valor;
     private Status status;
@@ -16,9 +14,6 @@ public class SaidaDespesaDTO {
 
     public Colaborador getColaborador() { return colaborador; }
     public void setColaborador(Colaborador colaborador) { this.colaborador = colaborador; }
-
-    public Projeto getProjeto() { return projeto; }
-    public void setProjeto(Projeto projeto) { this.projeto = projeto; }
 
     public String getDescricao() { return descricao; }
     public void setDescricao(String descricao) { this.descricao = descricao; }
@@ -33,7 +28,6 @@ public class SaidaDespesaDTO {
         SaidaDespesaDTO saidaDespesaDTO = new SaidaDespesaDTO();
 
         saidaDespesaDTO.setColaborador(despesa.getColaborador());
-        saidaDespesaDTO.setProjeto(despesa.getProjeto());
         saidaDespesaDTO.setDescricao(despesa.getDescricao());
         saidaDespesaDTO.setValor(despesa.getValor());
         saidaDespesaDTO.setStatus(despesa.getStatus());
