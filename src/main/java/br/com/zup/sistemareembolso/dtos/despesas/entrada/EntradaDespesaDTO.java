b0b8a784e23553ec1453993a586c1dba6aa0d642
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-05-12 14:07:05 -0300

    Alterar nome do atributo ID na DTO de entrada da entidade Despesa

--- src/main/java/br/com/zup/sistemareembolso/dtos/despesas/entrada/EntradaDespesaDTO.java
+++ src/main/java/br/com/zup/sistemareembolso/dtos/despesas/entrada/EntradaDespesaDTO.java
@@ -22,7 +22,7 @@ public class EntradaDespesaDTO {
     private int projetoId;
 
     @Positive
-    private Integer codCategoria;
+    private Integer categoriaId;
 
     @NotNull
     private NotaFiscalDTO notaFiscal;
@@ -32,7 +32,6 @@ public class EntradaDespesaDTO {
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
@@ -40,7 +39,6 @@ public class EntradaDespesaDTO {
     public double getValor() {
         return valor;
     }
-
     public void setValor(double valor) {
         this.valor = valor;
     }
@@ -48,23 +46,16 @@ public class EntradaDespesaDTO {
     public int getProjetoId() {
         return projetoId;
     }
-
     public void setProjetoId(int projetoId) {
         this.projetoId = projetoId;
     }
 
-    public Integer getCodCategoria() {
-        return codCategoria;
-    }
-
-    public void setCodCategoria(Integer codCategoria) {
-        this.codCategoria = codCategoria;
-    }
+    public Integer getCategoriaId() { return categoriaId; }
+    public void setCategoriaId(Integer categoriaId) { this.categoriaId = categoriaId; }
 
     public NotaFiscalDTO getNotaFiscal() {
         return notaFiscal;
     }
-
     public void setNotaFiscal(NotaFiscalDTO notaFiscal) {
         this.notaFiscal = notaFiscal;
     }
@@ -85,7 +76,7 @@ public class EntradaDespesaDTO {
         despesa.setValor(this.valor);
 
         Categoria categoria = new Categoria();
-        categoria.setCodCategoria(this.codCategoria);
+        categoria.setId(this.categoriaId);
 
         despesa.setCategoria(categoria);
         despesa.setNotaFiscal(notaFiscal.converterDTOParaNotaFiscal());
