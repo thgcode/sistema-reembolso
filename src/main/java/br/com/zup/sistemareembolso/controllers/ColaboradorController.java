65fe498d892bed9a3136eb9edd34abab279bef04
Thiago Seus <thiago.seus@yahoo.com.br>
2021-05-13 11:47:07 -0300

    Alterado o endpoint do controller para zuppers para manter a uniformidade

--- src/main/java/br/com/zup/sistemareembolso/controllers/ColaboradorController.java
+++ src/main/java/br/com/zup/sistemareembolso/controllers/ColaboradorController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/zupper/")
+@RequestMapping("zuppers/")
 public class ColaboradorController {
 
     @Autowired
